28e4543df1ac90d225661f909da2d2c4cbf44721
Arlene Joy Nacion <arlenejoy.nacion777@gmail.com>
2023-12-25 16:26:16 +0800

    Update ProjectItems.jsx

--- personal-port/src/components/ProjectItems.jsx
+++ personal-port/src/components/ProjectItems.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProjectItems = ({img, title}) => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl 
-    shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-pink-300'>
+    shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-slate-300'>
       <img src={img} alt="/" className='rounded-xl group-hover:opacity-10'/>
       <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl font-bold text-gray-600 tracking-wider text-center'> 
