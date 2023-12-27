9ee476241043bbfbf8a097e44c7de48479129a79
Yumeko <arlenejoy.nacion777@gmail.com>
2023-12-27 14:56:31 +0800

    fix: responsiveness

--- personal-port/src/components/CertItems.jsx
+++ personal-port/src/components/CertItems.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React from 'react'
 
 const CertItems = ({ year, title, duration, description, link }) => {
   return (
-    <ol className='flex flex-col md:flex-row relative border-l border-l border-stone-500 font-bold text-white/50'>
+    <ol className='flex flex-col md:flex-row relative border-l border-stone-500 font-bold text-white/50'>
       <li className='mb-10 ml-4'>
-        <div className='absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white '/>
-        <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
-          <span className='inline-block px-2 py-1 font-semibold text-black bg-white/70 rounded-md'>{year}</span>
-          <span className='text-lg font-semibold text-white/70'>{title}</span>
-          <span className='my-2 text-base font-normal text-stone-500'>{duration}</span>
+        <div className='absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white' />
+        <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm '>
+          <span className='inline-block px-2 py-1 font-semibold text-black bg-white/70 rounded-md md:inline-block'>
+            {year}
+          </span>
+          <span className='text-lg font-semibold text-white/70 md:inline-block'>{title}</span>
+          <span className='my-2 text-base font-normal text-stone-500 md:inline-block'>{duration}</span>
         </p>
-        <p>{description}</p>
-        <a href={link} className='cursor-pointer underline text-sky-900 hover:text-blue-200' target='_blank' rel='noopener noreferrer'>
+        <p className='text-sm md:text-base'>{description}</p>
+        <a
+          href={link}
+          className='cursor-pointer underline text-sky-900 hover:text-blue-200 block mt-2 md:inline-block text-sm md:text-base'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
           {link}
         </a>
       </li>
     </ol>
-  );
-};
+  )
+}
 
-export default CertItems;
+export default CertItems
--- personal-port/src/components/Projects.jsx
+++ personal-port/src/components/Projects.jsx
@@ -12,11 +12,14 @@ const Projects = () => {
         <h1 className='text-4xl font-bold text-center text-gray-400'>Projects</h1>
         <p className='text-center py-8 text-lg font-semibold text-white/70'> Greetings! I'm thrilled to present my diverse collection
          of projects that showcase my passion for web development. </p>
-        <div className='text-center mb-20 font-semibold'>
-         <a class='border-solid border-2 border-sky-950  text-gray-300 font-bold  p-2 rounded-full hover:bg-sky-950 hover:text-zinc-950 mr-4' href='#'>Web Development</a>
-         <a class='border-solid border-2 border-sky-950  text-gray-300 font-bold  p-2 rounded-full hover:bg-sky-950 hover:text-zinc-950 mr-4' href='#'>Mobile Development</a>
-         <a class='border-solid border-2 border-sky-950  text-gray-300 font-bold  p-2 rounded-full hover:bg-sky-950 hover:text-zinc-950 mr-4' href='#'>User Interface Designs</a>
-        </div>
+         <div className='text-center mb-20 font-semibold mx-auto md:flex md:flex-row md:justify-center md:gap-4'>
+  <a className='p-4 block mb-2 text-gray-300 font-bold py-2 rounded-full border-2 border-sky-950 hover:bg-sky-950 hover:text-zinc-950' href='#'>Web Development</a>
+  <a className='p-4 block mb-2 text-gray-300 font-bold py-2 rounded-full border-2 border-sky-950 hover:bg-sky-950 hover:text-zinc-950' href='#'>Mobile Development</a>
+  <a className='p-4 block mb-2 text-gray-300 font-bold py-2 rounded-full border-2 border-sky-950 hover:bg-sky-950 hover:text-zinc-950' href='#'>User Interface Designs</a>
+</div>
+
+
+
 
          <div className='grid sm:grid-cols-2 gap-12'>
             <ProjectItems img={MediZoneImg} title='MediZone'/>
