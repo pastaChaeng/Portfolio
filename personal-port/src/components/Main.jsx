729a911974cb229d8c4bb2b59ba57d04d33a4fc2
Yumeko <arlenejoy.nacion777@gmail.com>
2023-12-27 16:30:23 +0800

    feat: add loading & update

--- personal-port/src/components/Main.jsx
+++ personal-port/src/components/Main.jsx
@@ -1,53 +1,86 @@
-import React from 'react'
-import {TypeAnimation} from 'react-type-animation'
-import {FaTwitter, FaFacebookF, FaInstagram, FaGithub, FaLinkedinIn} from 'react-icons/fa'
+import React, { useState, useEffect } from 'react'
+import { FaSpinner } from 'react-icons/fa'
+import { TypeAnimation } from 'react-type-animation'
+import { FaTwitter, FaFacebookF, FaInstagram, FaGithub, FaLinkedinIn } from 'react-icons/fa'
 import MainItems from './MainItems'
 import Arlene from '../assets/leni.png'
 import Contact from './Contact'
 
+const LoadingScreen = () => {
+  return (
+    <div className='flex items-center justify-center w-screen h-screen bg-black fixed top-0 left-0 z-50'>
+     <img className='logo-heartbeat' src='https://i.pinimg.com/736x/0d/c8/2a/0dc82a78d504daea497e467c169270e4.jpg' alt='logo' width='150px' height='150px' />
+    </div>
+  )
+}
+
 const Main = () => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const delay = 1000
+
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, delay)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
+
   return (
     <div id='main'>
-    <img className='w-full h-screen object-cover object-left ' src="https://i.pinimg.com/564x/c4/49/6f/c4496f1147746d13b4c1641682b75912.jpg" alt="pix"></img>
-    <div className='w-full h-screen absolute top-0 left-0 bg-black/80'>
-        <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
-       <div className='hover:scale-110 ease-in duration-200 text-gray-300'> 
-       <MainItems img={Arlene}/>
-       </div>
-           <h1 className='sm:text-5xl text-4xl font-bold text-gray-400'> Arlene Joy Nacion </h1>
-           <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-500'> I'm a<TypeAnimation
-      sequence={[
-        'UI/UX Designer 🎨',
-        1000, 
-        'Tech Enthusiast 💻',
-        1000,
-        'Programmer 💻',
-        1000,
-        'Developer 💻',
-        1000
-      ]}
-      wrapper="span"
-      speed={50}
-      style={{ fontSize: '1em', paddingLeft: '5px' }}
-      repeat={Infinity}
-    /></h2>
-    <div className='flex justify-between pt-6 max-w-[200px] w-full text-gray-600'>
-        <a href="https://www.facebook.com/rlenesEy.7"><FaFacebookF className='cursor-pointer hover:text-blue-900 hover:scale-110' size={20} /></a>
-        <a href="https://twitter.com/rlenezy"><FaTwitter className='cursor-pointer hover:text-blue-600 hover:scale-110' size={20}/></a>
-        <a href="https://www.instagram.com/are__lean___/"><FaInstagram className='cursor-pointer hover:text-pink-600 hover:scale-110' size={20}/></a>
-        <a href="https://github.com/pastaChaeng"><FaGithub className='cursor-pointer hover:text-violet-950 hover:scale-110' size={20}/></a>
-        <a href="https://www.linkedin.com/in/nacion-arlene-joy-e-690137276/"><FaLinkedinIn className='cursor-pointer hover:text-blue-500 hover:scale-110' size={20}/></a>
-    </div>
-   <a href='#contact'>
-   <button className='border-solid border-2 border-sky-950 text-gray-300 font-bold py-2 px-4 rounded-full hover:bg-sky-950 hover:text-zinc-950 mt-6'>
-            Get in touch
-          </button>
-   </a>
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <div>
+          <img
+            className='w-full h-screen object-cover object-left'
+            src='https://i.pinimg.com/564x/c4/49/6f/c4496f1147746d13b4c1641682b75912.jpg'
+            alt='pix'
+          />
+          <div className='w-full h-screen absolute top-0 left-0 bg-black/80'>
+            <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
+              <div className='hover:scale-110 ease-in duration-200 text-gray-300'>
+                <MainItems img={Arlene} />
+              </div>
+              <h1 className='sm:text-5xl text-4xl font-bold text-gray-400'> Arlene Joy Nacion </h1>
+              <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-500'> I'm a
+                <TypeAnimation
+                  sequence={[
+                    'UI/UX Designer 🎨',
+                    1000,
+                    'Tech Enthusiast 💻',
+                    1000,
+                    'Programmer 💻',
+                    1000,
+                    'Developer 💻',
+                    1000
+                  ]}
+                  wrapper='span'
+                  speed={50}
+                  style={{ fontSize: '1em', paddingLeft: '5px' }}
+                  repeat={Infinity}
+                />
+              </h2>
+              <div className='flex justify-between pt-6 max-w-[200px] w-full text-gray-600'>
+                <a href='https://www.facebook.com/rlenesEy.7'><FaFacebookF className='cursor-pointer hover:text-blue-900 hover:scale-110' size={20} /></a>
+                <a href='https://twitter.com/rlenezy'><FaTwitter className='cursor-pointer hover:text-blue-600 hover:scale-110' size={20} /></a>
+                <a href='https://www.instagram.com/are__lean___/'><FaInstagram className='cursor-pointer hover:text-pink-600 hover:scale-110' size={20} /></a>
+                <a href='https://github.com/pastaChaeng'><FaGithub className='cursor-pointer hover:text-violet-950 hover:scale-110' size={20} /></a>
+                <a href='https://www.linkedin.com/in/nacion-arlene-joy-e-690137276/'><FaLinkedinIn className='cursor-pointer hover:text-blue-500 hover:scale-110' size={20} /></a>
+              </div>
+              <a href='#contact'>
+                <button className='border-solid border-2 border-sky-950 text-gray-300 font-bold py-2 px-4 rounded-full hover:bg-sky-950 hover:text-zinc-950 mt-6'>
+                  Get in touch
+                </button>
+              </a>
+            </div>
+          </div>
         </div>
-         </div>
+      )}
     </div>
-
-      
   )
 }
 
--- personal-port/src/components/SideNav.jsx
+++ personal-port/src/components/SideNav.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlinePhone, AiOutlineFolderOpen, AiOutlineTrophy, AiOutlineForm } from 'react-icons/ai'
+import React, { useState } from 'react';
+import { AiOutlineMenu, AiOutlineHome, AiOutlineFolderOpen, AiOutlineTrophy, AiOutlineForm, AiOutlinePhone } from 'react-icons/ai';
 import {GrProjects} from 'react-icons/gr'
 
 const SideNav = () => {
@@ -9,36 +9,42 @@ const SideNav = () => {
     setNav(!nav);
   };
 
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+      setNav(false); 
+    }
+  };
+
   return (
     <div>
-      <AiOutlineMenu onClick={handleNav} className="absolute top-4 right-4 z-[90] md:hidden text-white" />
-      { nav ? (
-        <div className="fixed w-full h-screen bg-black/90 flex flex-col justify-center items-center z-20">
-          <a href="#main" className="w-[60%] flex justify-center items-center rounded-full shadow-lg bg-gray-700 shadow-gray-700 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
-            <AiOutlineHome size={20} />
-            <span className="pl-4">Home</span>
-          </a>
-          <a href="#projects" className="w-[60%] flex justify-center items-center rounded-full shadow-lg bg-gray-600 shadow-gray-700 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
-            <AiOutlineFolderOpen size={20} />
-            <span className="pl-4">Projects</span>
-          </a>
-          <a href="#certs" className="w-[60%] flex justify-center items-center rounded-full shadow-lg bg-gray-500 shadow-gray-700 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
-            <AiOutlineTrophy size={20} />
-            <span className="pl-4">Certificates</span>
-          </a>
-          <a href="#resume" className="w-[60%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 shadow-gray-700 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
-            <AiOutlineForm size={20} />
-            <span className="pl-4">Resume</span>
-          </a>
-          <a href="#contact" className="w-[60%] flex justify-center items-center rounded-full shadow-lg bg-gray-300 shadow-gray-700 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
-            <AiOutlinePhone size={20} />
-            <span className="pl-4">Contact Me</span>
-          </a>
+<AiOutlineMenu onClick={handleNav} className="absolute top-4 right-4 z-[90] md:hidden text-white cursor-pointer text-3xl" />
+      {nav && (
+        <div className="fixed w-full h-screen bg-black bg-opacity-90 flex flex-col justify-center items-center z-20">
+          <button onClick={() => scrollToSection('main')} className="w-[60%] flex justify-center items-center rounded-full shadow-lg bg-gray-700 shadow-gray-700 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
+            <AiOutlineHome size={20} className="mr-2" />
+            <span>Home</span>
+          </button>
+          <button onClick={() => scrollToSection('projects')} className="w-[60%] flex justify-center items-center rounded-full shadow-lg bg-gray-600 shadow-gray-700 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
+            <AiOutlineFolderOpen size={20} className="mr-2" />
+            <span>Projects</span>
+          </button>
+          <button onClick={() => scrollToSection('certs')} className="w-[60%] flex justify-center items-center rounded-full shadow-lg bg-gray-500 shadow-gray-700 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
+            <AiOutlineTrophy size={20} className="mr-2" />
+            <span>Certificates</span>
+          </button>
+          <button onClick={() => scrollToSection('resume')} className="w-[60%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 shadow-gray-700 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
+            <AiOutlineForm size={20} className="mr-2" />
+            <span>Resume</span>
+          </button>
+          <button onClick={() => scrollToSection('contact')} className="w-[60%] flex justify-center items-center rounded-full shadow-lg bg-gray-300 shadow-gray-700 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200">
+            <AiOutlinePhone size={20} className="mr-2" />
+            <span>Contact Me</span>
+          </button>
         </div>
-      ) : (
-        ''
       )}
-      <div className='md:block hidden fixed top-[25%] z-10'>
+     <div className='md:block hidden fixed top-[25%] z-10'>
         <div className='flex flex-col'>
           <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-700 m-2 p-4 cursor-pointer hover:scale-110 bg-gray-700
           ease-in duration-300' name="Home">
